f30523df34dcc9691d0fd833380bf72e04b7a728
tensechsprt <tensechsprt@gmail.com>
2024-10-17 08:49:30 +0530

    fixes

--- app/components/homepage/blog/index.jsx
+++ app/components/homepage/blog/index.jsx
@@ -2,8 +2,50 @@
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 import BlogCard from './blog-card';
+import blog1 from '@/public/blogs/1.png';
+import blog2 from '@/public/blogs/2.png';
+import blog3 from '@/public/blogs/3.png';
+import blog4 from '@/public/blogs/4.png';
+import blog5 from '@/public/blogs/5.png';
 
-function Blog({ blogs }) {
+function Blog() {
+  const blogs = [
+    {
+      image:blog1,
+      title:"My Journey into Learning Next.js: A 3-Month Transformation",
+      description:"Three months ago, I hadn't even touched React.js, let alone Next.js. My background was firmly rooted in JavaScript, TypeScript, and Angular, where I was familiar with the fundamentals and had worked extensively. However, React.js and Next.js were completely new territories for me. This is the story of how I transitioned from having zero experience in React.js to gaining hands-on practice in Next.js, a framework that has significantly expanded my capabilities as a developer.",
+      date:"September 3, 2024",
+      link:"https://www.linkedin.com/pulse/my-journey-learning-nextjs-3-month-transformation-aakarshit-giri-clkac/"
+    },
+    {
+      image:blog2,
+      title:"System Design: Laying the Groundwork for Scalable and Efficient Systems",
+      description:"Building large-scale software systems that can handle high traffic volumes and scale efficiently is a complex challenge. Proper system design from the outset is crucial to ensure your application can grow and adapt as needed. In this article, we'll cover some key principles and strategies for designing scalable and efficient systems.",
+      date:"May 23, 2024",
+      link:"https://www.linkedin.com/pulse/system-design-laying-groundwork-scalable-efficient-systems-giri-x4kfc/"
+    },
+    {
+      image:blog3,
+      title:"Types of Backend Web Architecture with Node.js",
+      description:"Backend web architecture encompasses various structures and patterns for organizing server-side components. In the Node.js ecosystem, developers leverage different architectures to build scalable, maintainable, and efficient web applications. ",
+      date:"March 22, 2024",
+      link:"https://www.linkedin.com/pulse/types-backend-web-architecture-nodejs-aakarshit-giri-af4hc/"
+    },
+    {
+      image:blog4,
+      title:"Securing Your Socket.IO Chat APIs: Best Practices and Implementation Guide",
+      description:"In today's interconnected world, real-time communication is essential for countless applications, ranging from chat applications to collaborative tools. Socket.IO has emerged as a powerful tool for enabling real-time, bidirectional communication between clients and servers. However, ensuring the security of these chat APIs is paramount to protect sensitive data and maintain user privacy. In this article, we'll explore best practices for securing Socket.IO chat APIs and provide sample code snippets for implementation.",
+      date:"March 15, 2024",
+      link:"https://www.linkedin.com/pulse/securing-your-socketio-chat-apis-best-practices-guide-aakarshit-giri-kxekc/"
+    },
+    {
+      image:blog5,
+      title:"Exploring Real-Time Communication with Socket.IO and Node.js",
+      description:"In the dynamic landscape of web development, real-time communication has become a crucial aspect of creating engaging and interactive applications. One powerful tool that facilitates this is Socket.IO, a JavaScript library that enables bidirectional, event-based communication between clients and servers. In this article, we'll delve into the fundamentals of Socket.IO, demonstrate its integration with Node.js, and explore multiple use case examples.",
+      date:"November 21, 2023",
+      link:"https://www.linkedin.com/pulse/exploring-real-time-communication-socketio-nodejs-aakarshit-giri-hjxkf/"
+    }
+  ]
 
   return (
     <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
@@ -27,22 +69,11 @@ function Blog({ blogs }) {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
-          blogs.slice(0, 6).map((blog, i) => (
-            blog?.cover_image &&
+          blogs.map((blog, i) => (
+        
             <BlogCard blog={blog} key={i} />
           ))
         }
-      </div>
-
-      <div className="flex justify-center  mt-5 lg:mt-12">
-        <Link
-          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-          role="button"
-          href="/blog"
-        >
-          <span>View More</span>
-          <FaArrowRight size={16} />
-        </Link>
       </div>
     </div>
   );
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "I am a software engineer with over 2 years of experience, primarily focused on developing and enhancing travel portals. I have successfully delivered end-to-end projects, including the integration of hotel suppliers and the development of B2B and B2C platforms with wallet-related features. My work includes creating a robust B2B dashboard that allows for efficient client management and enables the launch of white-label sites within a week. Additionally, I developed a comprehensive logs management dashboard that merges Kibana and Grafana visualizations to monitor and analyze system performance across the platform.",
   email: 'aakarshitgiri1998@gmail.com',
   phone: '+91-7081543730',
-  address: 'Noida, Uttar Pradesh, Inida, 201301',
+  address: 'Noida, Uttar Pradesh, India, 201301',
   github: 'https://github.com/aakarshitgiri',
   facebook: 'https://www.facebook.com/aakarshit.giri/',
   linkedIn: 'https://www.linkedin.com/in/aakarshitgiri/',
